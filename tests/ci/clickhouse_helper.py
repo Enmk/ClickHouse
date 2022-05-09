c7429ab5e0df95b7e92082f7d547c2014b84a79b
Arthur Passos <arthur.ti@outlook.com>
2022-05-09 13:34:19 -0300

    remove extra colon

--- tests/ci/clickhouse_helper.py
+++ tests/ci/clickhouse_helper.py
@@ -15,7 +15,7 @@ class ClickHouseHelper:
                 "X-ClickHouse-User": get_parameter_from_ssm(
                     "clickhouse-test-stat-login2"
                 ),
-                "X-ClickHouse-Key": : get_parameter_from_ssm(
+                "X-ClickHouse-Key": get_parameter_from_ssm(
                     "clickhouse-test-stat-password"
                 ),
             }
