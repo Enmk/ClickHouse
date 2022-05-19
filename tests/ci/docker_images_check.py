f42d28d174b66125397d2b100f0cc781507f360c
Arthur Passos <arthur.ti@outlook.com>
2022-05-19 09:20:37 -0300

    Add TODO to fix docker base image argument

--- tests/ci/docker_images_check.py
+++ tests/ci/docker_images_check.py
@@ -247,6 +247,7 @@ def build_and_push_one_image(
         cmd = (
             "docker buildx build --builder default "
             f"--label build-url={GITHUB_RUN_URL} "
+#           TODO: fix providing base image
 #           Commented out as a workaround to make it work with Altinity's dockerhub account
 #             f"{from_tag_arg}"
             # A hack to invalidate cache, grep for it in docker/ dir
