cce53c367ee416563978e2ac5d472671685e2946
strtgbb <146047128+strtgbb@users.noreply.github.com>
2025-05-13 12:09:54 -0400

    configure altinity s3 buckets

---
+++ ci/settings/altinity_overrides.py
@@ -0,0 +1,55 @@
+S3_BUCKET_NAME = "altinity-build-artifacts"
+S3_REPORT_BUCKET_NAME = "altinity-test-reports"
+S3_BUCKET_HTTP_ENDPOINT = "altinity-build-artifacts.s3.amazonaws.com"
+S3_REPORT_BUCKET_HTTP_ENDPOINT = "s3.amazonaws.com/altinity-test-reports"
+
+
+class RunnerLabels:
+    STYLE_CHECK_AMD = ["self-hosted", "altinity-on-demand", "altinity-style-checker"]
+    STYLE_CHECK_ARM = [
+        "self-hosted",
+        "altinity-on-demand",
+        "altinity-style-checker-aarch64",
+    ]
+
+
+MAIN_BRANCH = "antalya"
+S3_ARTIFACT_PATH = S3_BUCKET_NAME
+CI_CONFIG_RUNS_ON = RunnerLabels.STYLE_CHECK_ARM
+
+ENABLE_MULTIPLATFORM_DOCKER_IN_ONE_JOB = False
+DOCKER_BUILD_AND_MERGE_RUNS_ON = RunnerLabels.STYLE_CHECK_AMD
+DOCKER_BUILD_ARM_RUNS_ON = RunnerLabels.STYLE_CHECK_ARM
+
+CACHE_S3_PATH = f"{S3_BUCKET_NAME}/ci_ch_cache"
+HTML_S3_PATH = S3_REPORT_BUCKET_NAME
+S3_BUCKET_TO_HTTP_ENDPOINT = {
+    S3_BUCKET_NAME: S3_BUCKET_HTTP_ENDPOINT,
+    S3_REPORT_BUCKET_NAME: S3_REPORT_BUCKET_HTTP_ENDPOINT,
+}
+ENABLE_ARTIFACTS_REPORT = True
+
+COMPRESS_THRESHOLD_MB = 32
+TEXT_CONTENT_EXTENSIONS = [".txt", ".log", ".err", ".out", ".tsv", ".csv", ".json"]
+
+# DOCKERHUB_USERNAME = "robotclickhouse"
+# DOCKERHUB_SECRET = "dockerhub_robot_password"
+
+CI_DB_DB_NAME = "gh-data"
+CI_DB_TABLE_NAME = "checks"
+SECRET_CI_DB_URL = "CHECKS_DATABASE_HOST"
+SECRET_CI_DB_USER = "CHECKS_DATABASE_USER"
+SECRET_CI_DB_PASSWORD = "CHECKS_DATABASE_PASSWORD"
+
+# USE_CUSTOM_GH_AUTH = True
+# SECRET_GH_APP_ID: str = "woolenwolf_gh_app.clickhouse-app-id"
+# SECRET_GH_APP_PEM_KEY: str = "woolenwolf_gh_app.clickhouse-app-key"
+
+INSTALL_PYTHON_REQS_FOR_NATIVE_JOBS = ""
+
+DISABLED_WORKFLOWS = [
+    "new_pull_request.py",
+]
+
+DEFAULT_LOCAL_TEST_WORKFLOW = "pull_request.py"
+READY_FOR_MERGE_CUSTOM_STATUS_NAME = "Mergeable Check"
